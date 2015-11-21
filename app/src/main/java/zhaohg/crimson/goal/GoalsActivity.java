0fbba3309c89af4aa2b45b1cc843a2e29fd2aca3
CyberZHG <CyberZHG@gmail.com>
2015-11-21 15:20:49 +0800

    Use FloatingActionButton.
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- app/src/main/java/zhaohg/crimson/goal/GoalsActivity.java
+++ app/src/main/java/zhaohg/crimson/goal/GoalsActivity.java
@@ -3,15 +3,11 @@ package zhaohg.crimson.goal;
 import java.util.ArrayList;
 
 import android.content.Intent;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBar;
+import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 
 import zhaohg.crimson.R;
@@ -38,6 +34,16 @@ public class GoalsActivity extends AppCompatActivity {
             }
         });
 
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.action_add);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(GoalsActivity.this, NewGoalActivity.class);
+                startActivity(intent);
+            }
+        });
+
         final SlidingTabLayout slidingTab = (SlidingTabLayout) findViewById(R.id.slidingTab);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         ArrayList<SlidingFragment> fragments = new ArrayList<>();
@@ -48,30 +54,6 @@ public class GoalsActivity extends AppCompatActivity {
         viewPager.setOffscreenPageLimit(fragments.size());
         viewPager.setAdapter(pagerAdapter);
         slidingTab.setViewPager(viewPager);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_goals, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                this.finish();
-                break;
-            case R.id.menu_item_new_goal: {
-                    Intent intent = new Intent();
-                    intent.setClass(GoalsActivity.this, NewGoalActivity.class);
-                    startActivity(intent);
-                }
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/zhaohg/crimson/main/MainActivity.java
+++ app/src/main/java/zhaohg/crimson/main/MainActivity.java
@@ -7,16 +7,11 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
